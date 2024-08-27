481fca9dff9d7795c485626b365b1a5c9a6bb7d2
joseowino <joeoowino@gmail.com>
2024-08-27 16:21:40 +0300

    Dog Years

---
+++ dog-years.js
@@ -0,0 +1,20 @@
+function dogYears(planet, age) {
+    const planetYrs = {
+        earth: 1.0,
+        mercury: 0.2408467,
+        venus: 0.61519726,
+        mars: 1.8808158,
+        jupiter: 11.862615,
+        saturn: 29.447498,
+        uranus: 84.016846,
+        neptune: 164.79132
+    }
+
+    const SEC_PER_YEAR = 31_557_600;
+    const DOG_YEARS = 7
+    const dogAge = age * planetYrs[planet] * DOG_YEARS / SEC_PER_YEAR
+
+
+    return Math.round(dogAge * 100) / 100
+
+}
