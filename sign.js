cf7c5106e374adbc2102a2f3111b5f93daffd036
joseowino <joeoowino@gmail.com>
2024-08-28 11:56:30 +0300

    signs

--- sign.js
+++ sign.js
@@ -13,8 +13,8 @@ function sameSign(a, b) {
         return true
     }
 
-    if (a = 0) {
-        if (b = 0) {
+    if (a > 0) {
+        if (b > 0) {
             return true
         } else {
             return false
