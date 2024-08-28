b3f5dcb4abd0f60489796cbd3406ef7fa6493625
joseowino <joeoowino@gmail.com>
2024-08-28 11:45:28 +0300

    signs

---
+++ sign.js
@@ -0,0 +1,29 @@
+function sign(n) {
+    if (n === 0) {
+        return 0
+    } else if (n < 0) {
+        return -1
+    } else {
+        return 1
+    }
+}
+
+function sameSign(a, b) {
+    if (a === b) {
+        return true
+    }
+
+    if (a >= 0) {
+        if (b >= 0) {
+            return true
+        } else {
+            return false
+        }
+    } else {
+        if (b < 0) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
