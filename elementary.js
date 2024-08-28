616bd0c847f80af7ec9f5b37835cecd706949691
joseowino <joeoowino@gmail.com>
2024-08-28 16:00:22 +0300

    Elementary Operators

--- elementary.js
+++ elementary.js
@@ -3,31 +3,19 @@ function multiply(a, b) {
         return 0
     }
 
-    let neg = 1
-
-    if (a < 0) {
-        a *= -1
-        if (b < 0) {
-            b *= -1
-        } else {
-           neg *= -1 
-        }
-    }
-
-    if (b < 0) {
-        b *= -1
-        if (a < 0) {
-            a *= -1
-        } else {
-           neg *= -1 
-        }
-    }
+    let isNeg = (a < 0) ^ (b < 0)
+    a = Math.abs(a)
+    b = Math.abs(b)
 
     let prod = a
     for (let i = 1; i < b; i++) {
         prod += a
     }
-    return prod * neg
+
+    if (isNeg) {
+        return -prod
+    }
+    return prod 
 }
 
 function divide(a, b) {
@@ -39,30 +27,17 @@ function divide(a, b) {
         return Infinity
     }
 
-
-    let neg = 1
-
-    if (a < 0) {
-        a *= -1
-        if (b < 0) {
-            b *= -1
-        } else {
-           neg *= -1 
-        }
-    }
-
-    if (b < 0) {
-        b *= -1
-        if (a < 0) {
-            a *= -1
-        } else {
-           neg *= -1 
-        }
-    }
+    let isNeg = (a < 0) ^ (b < 0)
+    a = Math.abs(a)
+    b = Math.abs(b)
 
     let div = 0
     for (; b <= a; div++) {
         a -= b
+    }
+
+    if (isNeg) {
+        return -div
     }
     return div
 }
@@ -74,6 +49,5 @@ function modulo(a, b) {
     return b - a
 }
 
-//console.log(multiply(-34, 78))
+console.log(divide(10, -2))
 
-console.log(2/0);
