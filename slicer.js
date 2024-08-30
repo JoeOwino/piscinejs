70e0fa436ef810159cf3649f1c2b195a47ba6eeb
joseowino <joeoowino@gmail.com>
2024-08-30 14:14:32 +0300

    Slice

---
+++ slicer.js
@@ -0,0 +1,14 @@
+function slice(input, start, end = input.length) {
+    const isString = typeof(input) === 'string'
+    let result = isString ? "" : []
+    
+    for (let i = start; i <= end; i++) {
+        if (isString) {
+            result += input(i)
+        } else {
+            result.push(input(i))
+        }
+    }
+
+    return result
+}
