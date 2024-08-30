d0d59e097ba6537de7837e9c558534a20cf631ef
joseowino <joeoowino@gmail.com>
2024-08-30 17:14:25 +0300

    Cut Corners

--- cut-corners.js
+++ cut-corners.js
@@ -1,19 +1,53 @@
+function modulo(a, b) {
+    if (b === 0) {
+        return Infinity
+    }
+
+    if (a === 0) {
+        return 0;
+    }
+
+    let isNeg = a < 0;
+    a = Math.abs(a);
+    b = Math.abs(b);
+
+    let prod = b;
+    while (prod <= a) {
+        prod += b;
+    }
+
+    let result = a - (prod - b);
+    return isNeg ? -result : result;
+}
+
 function round(n) {
-    let isCeil = n - parseInt(n) > 0.5
-    return isCeil ? parseInt(n) + 1 : parseInt(n)
+    let nDec = modulo(n, 1)
+    let nWhole = n - nDec
+
+    let isCeil = nDec > 0.5
+    return isCeil ? nWhole + 1 : nWhole
 }
 
 function ceil(n) {
-    let isCeil = n - parseInt(n) > 0
-    return isCeil ? parseInt(n) + 1 : parseInt(n)
+    let nDec = modulo(n, 1)
+    let nWhole = n - nDec
+
+    let isCeil = nDec > 0
+    return isCeil ? nWhole + 1 : nWhole
 }
 
 function floor(n) {
-    return parseInt(n)
+    let nDec = modulo(n, 1)
+    let nWhole = n - nDec
+
+    return nWhole
 }
 
 function trunc(n) {
-    return parseInt(n)
+    let nDec = modulo(n, 1)
+    let nWhole = n - nDec
+
+    return nWhole
 }
 
-console.log(floor(2.99))
+console.log(round(2.04))
