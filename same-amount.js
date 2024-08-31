6d7c429c4613f332dc777cb76e4c10cd51e2a8b9
JoeOwino <Joeoowino@gmail.com>
2024-08-31 20:51:03 +0300

    Same Amount

--- same-amount.js
+++ same-amount.js
@@ -1,5 +1,5 @@
 function  sameAmount(str, reg1, reg2) {
-    rexp1 = new RegExp(reg1, "g");
-    rexp2 = new RegExp(reg2, "g");
+    reg1 = new RegExp(reg1, "g");
+    reg2 = new RegExp(reg2, "g");
     return str.match(reg1).length === str.match(reg2).length 
 }
