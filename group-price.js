5c827e1f76a3c1fc67986c87b85dbbed1af39bf0
JoeOwino <Joeoowino@gmail.com>
2024-09-01 08:24:05 +0300

    Group Prices

---
+++ group-price.js
@@ -0,0 +1,11 @@
+function groupPrice(str) {
+    let prices = str.match(/(([A-Z]{3})|\$)([0-9]+\.[0-9]+)/g)
+    let arrPrice = [];
+    if (prices === null) return arrPrice
+    prices.forEach((price, i) => {
+        arrPrice.push([price]);
+        arrPrice[i].push(price.match(/[0-9]+/g)[0])
+        arrPrice[i].push(price.match(/[0-9]+/g)[1])
+    });
+    return arrPrice;
+}
